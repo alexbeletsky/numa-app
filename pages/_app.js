3b7541a76dc349e6a7da93d24172851402f54086
Alexander Beletsky <alexander.beletsky@gmail.com>
2022-10-27 16:16:16 +0200

    basic layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import { ChakraProvider } from '@chakra-ui/react';
 
 import { theme } from '../theme';
+import { Layout } from './layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <Component {...pageProps} />;
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </ChakraProvider>
   );
 }
