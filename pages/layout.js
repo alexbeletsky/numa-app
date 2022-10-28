389d7a609a1580cd7d54ce4b29805236f931fc2e
Alexander Beletsky <alexander.beletsky@gmail.com>
2022-10-28 17:28:59 +0200

    logo

--- pages/layout.js
+++ pages/layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-import { Box } from '@chakra-ui/react';
+import { Image, Box } from '@chakra-ui/react';
 
 export function Layout({ children }) {
   return (
     <Box minH="100vh" bg="gray.100">
-      <Box p={6}>{children}</Box>
+      <Image src="/numa.svg" height={10} pt={4} pb={2} pl={2} />
+      <Box p={2}>{children}</Box>
     </Box>
   );
 }
